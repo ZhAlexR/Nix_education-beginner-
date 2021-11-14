a251ebec14b1772edcf2e74416ece0655404d3c0
Zhukov Oleksandr <oleksandrzhukov@gmail.com>
2021-11-14 15:26:01 +0200

    Checked by Flake8

--- python_exercises/8_find_777.py
+++ python_exercises/8_find_777.py
@@ -4,7 +4,7 @@ import string
 
 def get_random_string(length):
     letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for i in range(length))
+    result_str = ''.join(random.choice(letters) for _ in range(length))
     return result_str
 
 
@@ -16,15 +16,14 @@ if __name__ == "__main__":
         random_list.append(get_random_string(10))
 
     search_list = random.sample(random_list, len(random_list))
-    
     counter = 0
     for elem in search_list:
-    	if elem == 777:
-    		print (f"The 777 was found in list, the index is {search_list.index(elem)}")
-    		break
+        if elem == 777:
+            print(f"The 777 was found in list, the index is {search_list.index(elem)}")
+            break
 
-    	elif counter >= 100:
-    		print ("100 elements were checked")
-    		break
-    	else:
-    		counter += 1
+        elif counter >= 100:
+            print("100 elements were checked")
+            break
+        else:
+            counter += 1
