6dad89f5bb98e40a309c4832b472084859e756b6
Zhukov Oleksandr <oleksandrzhukov@gmail.com>
2021-11-14 15:17:50 +0200

    Checked by Flake8

--- python_exercises/2_template_strings.py
+++ python_exercises/2_template_strings.py
@@ -1,13 +1,13 @@
 
 def greetings(first_name, last_name):
-	if first_name.isalpha() and last_name.isalpha():
-		return "Hello, {} {}".format(first_name, last_name)
-	else:
-		return "Sorry, but script works with alphabetic names only"
+    if first_name.isalpha() and last_name.isalpha():
+        return "Hello, {} {}".format(first_name, last_name)
+    else:
+        return "Sorry, but script works with alphabetic names only"
 
 
 if __name__ == '__main__':
-	first_name = input("Please, input your first name:\n")
-	last_name = input("Please, input your last name:\n")
+    first_name = input("Please, input your first name:\n")
+    last_name = input("Please, input your last name:\n")
 
-	print (greetings(first_name, last_name))
+    print(greetings(first_name, last_name))
