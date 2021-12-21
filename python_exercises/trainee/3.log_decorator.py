3cb3af88308667d97ed9af2f7e5b260a68ab45bc
Zhukov Oleksandr <oleksandrzhukov@gmail.com>
2021-12-21 22:03:05 +0200

    Modified: add new logging decorator
    In this case, I used the "logging" module to log errors

--- python_exercises/trainee/3.log_decorator.py
+++ python_exercises/trainee/3.log_decorator.py
@@ -1,5 +1,8 @@
 # Написать свой декоратор, который будет отлавливать ошибки, полученные в ходе выполнения обёрнутой
 # функции, логгировать их и делать raise отловленной ошибки
+import logging
+
+
 def log(func):
 	""" Loging errors """
 
@@ -13,10 +16,27 @@ def log(func):
 
 	return wrapper
 
-@log
+
+def logging_module(func):
+	""" Loging errors using logging module """
+
+	def func_wrapper(*args, **kwargs):
+		try:
+			func(*args, **kwargs)
+		except Exception as err:
+			logging.basicConfig(filename=f"{func.__name__}.log", format="%(levelname)s: %(message)s\n"
+																		"time: %(asctime)s", level=logging.ERROR)
+			logging.exception(err)
+			raise
+
+	return func_wrapper
+
+
+@logging_module
 def divide(a, b):
 	return a / b
 
 
 if __name__ == "__main__":
 	divide(5, 0)
+
